c547980c823d883b3656956d98189f1f31ddadcc
mikiPP <miki.palao.palmer@gmail.com>
2020-06-03 19:59:48 +0200

    fix(Navbar): Navbar is not routed anymore

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Index from './components/Index/index';
-import Navbar from './components/Navbar/navbar';
 
 class App extends Component {
   render() {
     let routes = (
       <Switch>
         <Route path="/auth" component={Auth}></Route>
-        <Route path="/navbar" component={Navbar}></Route>
         <Route path="/" exact component={Index}></Route>
         <Redirect to="/" />
       </Switch>
