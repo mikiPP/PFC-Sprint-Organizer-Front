02e3f98a231ccee9ff191d8523e0502b4b7b7b85
mikiPP <miki.palao.palmer@gmail.com>
2020-06-07 18:17:41 +0200

    feat(Project): Project action types have been added

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,5 +1,15 @@
+// Auth Action Types
+
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 export const SIGN_UP_START = 'SIGN_UP_START';
 export const AUTH_START = 'AUTH_START';
 export const AUTH_FAIL = 'AUTH_FAIL';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+
+// Project Action Types
+
+export const FETCH_IDS_START = 'FETCH_IDS_START';
+export const FETCH_IDS_SUCCESS = 'FETCH_IDS_SUCCESS';
+export const FETCH_PROJECT_START = 'FETCH_PROJECT_START';
+export const FETCH_PROJECTS_SUCCESS = 'FETCH_PROJECTS_SUCCESS';
+export const FETCH_FAIL = 'FETCH_FAIL';
