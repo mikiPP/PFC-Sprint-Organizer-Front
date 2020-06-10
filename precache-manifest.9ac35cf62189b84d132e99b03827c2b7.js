e820f1f61d0b79a5ca36b60a095b945b0843386d
mikiPP <miki.palao.palmer@gmail.com>
2020-06-11 01:28:54 +0200

    Updates

---
+++ precache-manifest.9ac35cf62189b84d132e99b03827c2b7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50fb3857202a214ccf4c3451ef6248b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88392fbe15c44531b2ec",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b212381b6bdc9e923b25",
+    "url": "/static/css/main.beebae2c.chunk.css"
+  },
+  {
+    "revision": "88392fbe15c44531b2ec",
+    "url": "/static/js/2.4a17c4bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4a17c4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b212381b6bdc9e923b25",
+    "url": "/static/js/main.e53a72bc.chunk.js"
+  },
+  {
+    "revision": "1c7712921a8daa60d64b",
+    "url": "/static/js/runtime-main.0cd4f629.js"
+  },
+  {
+    "revision": "541ea73e388dbeaa55de8277efa58552",
+    "url": "/static/media/dashboard.541ea73e.jpg"
+  },
+  {
+    "revision": "af320780ec20848c460667b0afe51ccd",
+    "url": "/static/media/logo.af320780.svg"
+  },
+  {
+    "revision": "0cfcdaf8af24dd4c063eedac601d06cd",
+    "url": "/static/media/logoLetters.0cfcdaf8.svg"
+  },
+  {
+    "revision": "f3214e42b60a6898d70f71c8f38e6d2e",
+    "url": "/static/media/productividad.f3214e42.png"
+  },
+  {
+    "revision": "64e9589d538f848310f5d0eb1f86ab4c",
+    "url": "/static/media/reunion.64e9589d.png"
+  },
+  {
+    "revision": "4c6d3314613c40a29a4a9921fcbc7a2e",
+    "url": "/static/media/working.4c6d3314.jpg"
+  }
+]);
